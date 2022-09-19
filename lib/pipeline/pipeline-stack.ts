b2dfdee30a63fad93156f02ba7509af7747c0a3a
Sean Cho <sean@Seans-MacBook-Pro.local>
2022-09-19 16:25:06 -0700

    Update github connection

--- lib/pipeline/pipeline-stack.ts
+++ lib/pipeline/pipeline-stack.ts
@@ -9,7 +9,9 @@ export class PipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'Sean-Pipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('chosh0615/aws-cdk-sample', 'main'),
+        input: CodePipelineSource.connection('chosh0615/aws-cdk-sample', 'main', {
+          connectionArn: "arn:aws:codestar-connections:ap-northeast-2:007816030679:connection/72aaa5b5-045e-4706-8093-b8e26b9ad84a"
+        }),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
