5c6473566a7b33ab27a6631c8349943ec1d10db1
Sean Cho <sean@Seans-MacBook-Pro.local>
2022-10-08 23:17:04 -0700

    use CODEBUILD_BUILD_NUMBER

--- lib/pipeline/pipeline-stack.ts
+++ lib/pipeline/pipeline-stack.ts
@@ -13,7 +13,7 @@ export class PipelineStack extends cdk.Stack {
         input: CodePipelineSource.connection('chosh0615/aws-cdk-sample', 'main', {
           connectionArn: "arn:aws:codestar-connections:ap-northeast-2:154533163640:connection/e1b82912-074e-4112-8c6c-f6527d227462"
         }),
-        commands: ['npm ci', 'npm run build', 'echo $CODEBUILD_SOURCE_VERSION', 'npx cdk synth']
+        commands: ['npm ci', 'npm run build', 'echo $CODEBUILD_BUILD_NUMBER', 'npx cdk synth']
       })
     });
 
